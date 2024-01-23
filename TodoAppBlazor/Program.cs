24ae30d8b84c348b99bd5325f23c3dac3c8c2945
Luan053 <luanrschulzz@gmail.com>
2024-01-22 23:32:01 -0300

    refactor: adicionando lib Radzen e alterando alguns componentes

--- TodoAppBlazor/Program.cs
+++ TodoAppBlazor/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<ITodoService, TodoService>();
 
+
+
 var app = builder.Build();
 
 
@@ -21,6 +23,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 
 app.UseHttpsRedirection();
 
